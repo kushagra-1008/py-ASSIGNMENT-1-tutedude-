9d97c58e6b422456771c0845f6ddd484db4f0cae
kushagra-1008 <kushagra301001@gmail.com>
2025-05-30 23:48:48 +0530

    Delete TASK-1.py

--- TASK-1.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter the First Number: "))
-num2 = int(input("Enter the Second Number: "))
-
-print(f"ADDITION: {num1+num2}")
-print(f"SUBTRACTION: {num1-num2}")
-print(f"MULTIPLICATION: {num1*num2}")
-print(f"DIVISION: {num1/num2}")
-
