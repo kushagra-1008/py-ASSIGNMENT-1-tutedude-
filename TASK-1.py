3d9d06ed3170c2c76cd59cbefd630db4c4f6b31e
kushagra-1008 <kushagra301001@gmail.com>
2025-05-30 23:11:57 +0530

    Add files via upload

---
+++ TASK-1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the First Number: "))
+num2 = int(input("Enter the Second Number: "))
+
+print(f"ADDITION: {num1+num2}")
+print(f"SUBTRACTION: {num1-num2}")
+print(f"MULTIPLICATION: {num1*num2}")
+print(f"DIVISION: {num1/num2}")
+
---
+++ TASK-2.py
@@ -0,0 +1,4 @@
+a = input("ENTER YOUR FIRST NAME: ")
+b = input("ENTER YOUR LAST NAME: ")
+
+print(f"Hello {a} {b}! Welcome to the Python Program.")
